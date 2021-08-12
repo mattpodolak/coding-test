fd8a2cc948d2b9a963646d47d962d25193175481
Matthew Podolak <mpodola2@gmail.com>
2021-08-12 10:15:16 -0400

    change logo colour

--- client/src/organisms/Loading.js
+++ client/src/organisms/Loading.js
@@ -4,7 +4,7 @@ export const Loading = () => {
   return (
     <div className="flex flex-col space-y-3 justify-center py-20">
       <div className="self-center">
-        <Spinner img="catsharklogo.png" imgAlt="catshark logo" />
+        <Spinner img="catsharklogo-purple.png" imgAlt="purple catshark logo" />
       </div>
       <h2 className="self-center text-2xl font-medium text-ownr-black">
         Loading
